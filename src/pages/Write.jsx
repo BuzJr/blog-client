e4dca9abafa39b2f0dbdaef6c428f64eca1956f7
BuzJr <pawlasnr@gmail.com>
2023-02-05 20:04:39 -0500

    Removed upload function for testing

--- src/pages/Write.jsx
+++ src/pages/Write.jsx
@@ -18,7 +18,7 @@ const Write = () => {
 
   const { currentUser } = useContext(AuthContext)
 
-  const upload = async () => {
+  /*const upload = async () => {
     try {
       const formData = new FormData();
       formData.append("file", file);
@@ -26,11 +26,11 @@ const Write = () => {
     } catch (err) {
       console.log(err);
     }
-  };
+  };*/
 
   const handleClick = async (e) => {
     e.preventDefault();
-    const imgUrl = await upload();
+    //const imgUrl = await upload();
 
     try {
       state? await axios.put(`https://classicsblogapi.herokuapp.com/api/posts/${state.id}`, {
