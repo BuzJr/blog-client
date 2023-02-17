005ddb2e5c90bb372ca798cc3d1db02f21fa0b60
BuzJr <pawlasnr@gmail.com>
2023-02-17 18:06:18 -0500

    Making image uploading local

--- src/index.js
+++ src/index.js
@@ -2,6 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { AuthContexProvider } from "./context/authContext";
+import multer from 'multer'
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, '/public/uploads')
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now()+file.originalname)
+  }
+})
+
+const upload = multer({storage})
+
+app.post('/api/upload', upload.single('file'), function (req, res){
+  const file = req.file
+  res.status(200).json(file.filename)
+})
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
