f2822ac5cf314635aca4fcb9f47634c7a756a021
BuzJr <pawlasnr@gmail.com>
2023-02-20 18:58:54 -0500

    Added message

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,6 +5,7 @@ const Footer = () => {
   return (
     <footer>
       <img src={Logo} alt=""/>
+      <span>If nothing shows up, the server hosting the backend (Heroku) is likely down, this is temporary and it will return</span>
       <span>powered by <b>React.js</b>.</span>
     </footer>
   )
